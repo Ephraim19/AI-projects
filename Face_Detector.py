b7748bb6b778157daadc628a3ac9b8f4bcd00303
Ephraim19 <machayoephraim@gmail.com>
2022-11-02 17:31:32 +0300

    Added video face detection

--- Face_Detector.py
+++ Face_Detector.py
@@ -3,18 +3,30 @@ import cv2
 #load data
 face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-#choose image to be detected
-img = cv2.imread('Ephraim.jpg')
+# #choose image to be detected
+#img = cv2.imread('Ephraim.jpg')
 #convert img to blacknwhite
-blacknwhite = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#Detect faces
-face_coord = face_data.detectMultiScale(blacknwhite)
-print(face_coord)
-#draw rectangle around the face
-for (x,y,w,h) in face_coord:
-    cv2.rectangle(img,(x,y),(x + w,y + h),(0,255,0),2)
-#show image
-cv2.imshow("This is Ephraim", img)
-cv2.waitKey()
 
-print("Code completed")
+webcam = cv2.VideoCapture(0)
+
+while True:
+    succesful_frame, frame = webcam.read()
+    blacknwhite = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    #Detect faces
+    face_coord = face_data.detectMultiScale(blacknwhite)
+
+    #draw rectangle around the face
+    for (x,y,w,h) in face_coord:
+        cv2.rectangle(frame,(x,y),(x + w,y + h),(0,255,0),2)
+
+    #show image
+    cv2.imshow("This is Ephraim", frame)
+    cv2.waitKey(1)
+
+
+
+
+
+
+# print("Code completed")
