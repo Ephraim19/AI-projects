e2bc76c70996028d47a35ee7ddccb9d1537f028e
Ephraim19 <machayoephraim@gmail.com>
2022-11-03 12:52:11 +0300

    Added ML survival probabilities for titanic passengers

---
+++ Car_and_Pedestrian_tracker.py
@@ -0,0 +1,6 @@
+import cv2
+
+img_file = "carss.jpeg"
+pretrained_file = "car_detector.xml"
+
+print("code printed")
--- Face_Detector.py
+++ Face_Detector.py
@@ -5,11 +5,14 @@ face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # #choose image to be detected
 #img = cv2.imread('Ephraim.jpg')
+#print(img)
+
 #convert img to blacknwhite
 
 webcam = cv2.VideoCapture(0)
 
 while True:
+
     succesful_frame, frame = webcam.read()
     blacknwhite = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
@@ -18,8 +21,8 @@ while True:
 
     #draw rectangle around the face
     for (x,y,w,h) in face_coord:
-        cv2.rectangle(frame,(x,y),(x + w,y + h),(0,255,0),2)
-
+        cv2.rectangle(frame,(x,y),(x + w,y + h),(0,255,0),5)
+    
     #show image
     cv2.imshow("This is Ephraim", frame)
     cv2.waitKey(1)
